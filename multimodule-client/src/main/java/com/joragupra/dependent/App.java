7cc801a07adc3c2936dcbd912e79f843908b2540
Jorge Agudo <joragupra@gmail.com>
2013-01-10 16:31:36 +0100

    Projects created.

---
+++ multimodule-client/src/main/java/com/joragupra/dependent/App.java
@@ -0,0 +1,13 @@
+package com.joragupra.dependent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
